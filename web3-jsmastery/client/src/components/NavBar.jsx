bd0c6ee1c67d872468963da8324b9bbebfd09351
Aleksandar Ristic <storiaca@gmail.com>
2022-08-28 12:54:07 +0200

    Start navbar component

--- web3-jsmastery/client/src/components/NavBar.jsx
+++ web3-jsmastery/client/src/components/NavBar.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+import logo from "../assets/logo.png";
+
+const NavbarItem = ({ title, classProps }) => {
+  return <li className={`mx-4`}></li>;
+};
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <nav className="w-full flex justify-between items-center p-4 md:justify-center">
+      <div className="flex-initial justify-center items-center md:flex-[0.5]">
+        <img src={logo} alt="Logo" className="w-32 cursor-ponter" />
+      </div>
+      <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
